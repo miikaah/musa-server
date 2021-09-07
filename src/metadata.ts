154edf79016aa7d8c39b57db22b4aa8e635de775
Miika Henttonen <miika.henttonen@gmail.com>
2021-09-07 14:33:49 +0300

    Add format information to metadata

--- src/metadata.ts
+++ src/metadata.ts
@@ -4,6 +4,12 @@ import UrlSafeBase64 from "./urlsafe-base64";
 
 const { MUSA_SRC_PATH = "" } = process.env;
 
+type FormatMetadata = {
+  bitrate: number;
+  duration: number;
+  sampleRate: number;
+};
+
 type CommonMetadata = {
   track: NumberOf;
   disk: NumberOf;
@@ -23,7 +29,9 @@ type CommonMetadata = {
 };
 
 type AudioMetadata = {
+  format: Partial<FormatMetadata>;
   native: {
+    ID3v1?: { id: string; value: string }[];
     "ID3v2.3"?: { id: string; value: string }[];
     "ID3v2.4"?: { id: string; value: string }[];
   };
@@ -31,7 +39,7 @@ type AudioMetadata = {
 };
 
 export const readMetadata = async (filepath: string): Promise<AudioMetadata> => {
-  let metadata = { native: { "ID3v2.3": [] }, common: {} };
+  let metadata = { format: {}, native: { "ID3v2.3": [] }, common: {} };
 
   try {
     // @ts-expect-error wrongly typed in music-metadata
@@ -60,6 +68,9 @@ export type Metadata = Partial<{
   genre: string[];
   dynamicRange: string;
   dynamicRangeAlbum: string;
+  bitrate: number;
+  duration: number;
+  sampleRate: number;
 }>;
 
 type NumberOf = { no: string | null; of: string | null };
@@ -67,16 +78,16 @@ type ReplayGain = { dB: number; ratio: number };
 
 export const getMetadata = async (id: string): Promise<Metadata> => {
   const audioPath = path.join(MUSA_SRC_PATH, UrlSafeBase64.decode(id));
-  const { native, common } = await readMetadata(audioPath);
-  const id3v2point3 = native["ID3v2.4"] || native["ID3v2.3"] || [];
+  const { format, native, common } = await readMetadata(audioPath);
+  const id3v2x = native["ID3v2.4"] || native["ID3v2.3"] || native["ID3v1"] || [];
   const id3v2 = {
-    dynamicRange: (id3v2point3.find((tag) => tag.id === "TXXX:DYNAMIC RANGE") || {}).value,
-    dynamicRangeAlbum: (id3v2point3.find((tag) => tag.id === "TXXX:ALBUM DYNAMIC RANGE") || {})
-      .value,
+    dynamicRange: (id3v2x.find((tag) => tag.id === "TXXX:DYNAMIC RANGE") || {}).value,
+    dynamicRangeAlbum: (id3v2x.find((tag) => tag.id === "TXXX:ALBUM DYNAMIC RANGE") || {}).value,
   };
 
-  console.log(native);
-  console.log(common);
+  console.log("format", format);
+  console.log("native", native);
+  console.log("common", common);
 
   const {
     track,
@@ -95,6 +106,7 @@ export const getMetadata = async (id: string): Promise<Metadata> => {
     albumartist: albumArtist,
     comment,
   } = common;
+  const { bitrate, duration, sampleRate } = format;
 
   const metadata = {
     track,
@@ -112,6 +124,9 @@ export const getMetadata = async (id: string): Promise<Metadata> => {
     composer,
     albumArtist,
     comment,
+    bitrate,
+    duration,
+    sampleRate,
     ...id3v2,
   };
 
