372d222981c790eb5eebd5eadb50f9878a7c21ae
Miika Henttonen <miika.henttonen@wunderdog.fi>
2021-09-26 15:43:24 +0300

    Enable CORS for *

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 export const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: "*" }));
 
 // Routes
 export * from "./api/hello";
