6d7712ba4d9ceb667355752a682f73a81825171c
Miika Henttonen <miika.henttonen@wunderdog.fi>
2021-09-26 15:46:03 +0300

    Set origin to false

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 export const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: false }));
 
 // Routes
 export * from "./api/hello";
