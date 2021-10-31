36b6fd4c36c1acfb39000414011f89e6018096ad
Miika Henttonen <miika.henttonen@wunderdog.fi>
2021-10-31 12:40:32 +0200

    Make theme api work with new schema

--- src/api/theme.ts
+++ src/api/theme.ts
@@ -1,41 +1,42 @@
 import { Request } from "express";
-import { Api, UrlSafeBase64 } from "musa-core";
+import { Api, DbTheme } from "musa-core";
 import { app } from "../api";
 
 app.get("/themes", async (_req, res) => {
   const themes = await Api.getAllThemes();
 
-  res.status(200).json(
-    themes.map(({ path_id, colors }) => ({
-      id: path_id,
-      colors,
-    }))
-  );
+  res.status(200).json(themes.map(toApiTheme));
 });
 
 app.get("/theme/:id", async (req: Request<{ id: string }>, res) => {
   const { id } = req.params;
-  const themeId = UrlSafeBase64.decode(id);
-  const theme = await Api.getTheme(themeId);
+  const theme = await Api.getTheme(id);
 
   if (!theme) {
     res.status(404).json({ message: "Not Found" });
     return;
   }
 
-  res.status(200).json({
-    id: theme.path_id,
-    colors: theme.colors,
-  });
+  res.status(200).json(toApiTheme(theme));
 });
 
 app.put("/theme/:id", async (req: Request<{ id: string }, unknown, { colors: unknown }>, res) => {
   const { id } = req.params;
-  const themeId = UrlSafeBase64.decode(id);
   const { colors } = req.body;
 
-  const newTheme = await Api.insertTheme(themeId, colors);
-  const { path_id } = newTheme;
+  const newTheme = await Api.insertTheme(id, colors);
 
-  res.status(200).json({ id: path_id, colors });
+  res.status(201).json(toApiTheme(newTheme));
 });
+
+app.delete("/theme/:id", async (req: Request<{ id: string }>, res) => {
+  const { id } = req.params;
+
+  await Api.removeTheme(id);
+
+  res.status(204).send();
+});
+
+function toApiTheme({ path_id, filename, colors }: DbTheme) {
+  return { id: path_id, filename, colors };
+}
