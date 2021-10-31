26a25a9588ef69f529fda688fcb7284dcffa09fe
Miika Henttonen <miika.henttonen@wunderdog.fi>
2021-10-31 13:21:59 +0200

    Add artist albums api

--- src/api/artist.ts
+++ src/api/artist.ts
@@ -11,3 +11,9 @@ app.get("/artist/:id", async (req: Request<{ id: string }>, res) => {
 app.get("/artists", async (_req, res) => {
   res.status(200).json(await Api.getArtists());
 });
+
+app.get("/artist-albums/:id", async (req, res) => {
+  const { id } = req.params;
+
+  res.status(200).json(await Api.getArtistAlbums(id));
+});
