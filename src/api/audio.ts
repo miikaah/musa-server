7218974681a3fa02b88b8c7d216fc85a4ff64705
Miika Henttonen <miika.henttonen@gmail.com>
2022-01-06 16:43:50 +0200

    Remove redundant getAudioById call

--- src/api/audio.ts
+++ src/api/audio.ts
@@ -5,7 +5,5 @@ import { app } from "../api";
 app.get("/audio/:id", async (req: Request<{ id: string }>, res) => {
   const { id } = req.params;
 
-  await Api.getAudioById({ id });
-
   res.status(200).json(await Api.getAudioById({ id }));
 });
