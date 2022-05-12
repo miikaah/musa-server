28da0d6d744b75aced78ffdba383366ffe2188f4
Miika Henttonen <miika.henttonen@gmail.com>
2022-05-12 16:26:17 +0300

    Add compression middleware

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
+import compression from "compression";
 
 export const app = express();
 
 app.use(express.json());
 app.use(cors({ origin: "*" }));
+app.use(compression());
 
 export * from "./api/find";
 export * from "./api/file";
