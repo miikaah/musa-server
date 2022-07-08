ca16d836a3df7b820803464ceb7b455e58d29791
Miika Henttonen <miika.henttonen@gmail.com>
2022-07-08 11:22:30 +0300

    Update musa-core to v7.1.1

--- src/api/find.ts
+++ src/api/find.ts
@@ -6,7 +6,7 @@ import { app } from "../api";
 app.get("/find/:query", async (req: Request<{ query: string }>, res) => {
   const { query } = req.params;
 
-  res.status(200).json(await Api.find({ query }));
+  res.status(200).json(await Api.find({ query, limit: 32 }));
 });
 
 app.get("/find-random", async (_req, res) => {
