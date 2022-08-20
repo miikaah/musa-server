8410c53edfac96d54ca58c0528399b3b0662c0a0
Miika Henttonen <miika.henttonen@gmail.com>
2022-08-20 14:42:22 +0300

    Add api for random search with locking search term + update musa-core

--- src/api/find.ts
+++ src/api/find.ts
@@ -12,3 +12,9 @@ app.get("/find/:query", async (req: Request<{ query: string }>, res) => {
 app.get("/find-random", async (_req, res) => {
   res.status(200).json(await Api.findRandom({ limit: 7 }));
 });
+
+app.get("/find-random/:query", async (req, res) => {
+  const { query } = req.params;
+
+  res.status(200).json(await Api.findRandom({ limit: 7, lockedSearchTerm: query }));
+});
