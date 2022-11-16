569306ef96123f7efd1c8d10b69382efe4609797
Miika Henttonen <miika.henttonen@wunderdog.fi>
2022-11-16 14:21:54 +0200

    Fix tests that broke because of ESM transpilation

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,11 @@ module.exports = {
   setupFiles: ["<rootDir>/.jest/setEnvVars.js"],
   testEnvironment: "node",
   testMatch: ["<rootDir>/src/**/*.spec.ts"],
+  transformIgnorePatterns: [
+    "node_modules/(?!(@miikaah/musa-core|music-metadata|strtok3|peek-readable|file-type|token-types)/)",
+  ],
   transform: {
-    "^.+\\.(t|j)sx?$": ["@swc/jest"],
+    "^.+\\.ts$": ["@swc/jest"],
+    "^.+\\.js$": ["babel-jest"],
   },
 };
